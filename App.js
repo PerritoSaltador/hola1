a31b0a48d22978f617fddf1754237f5a47170690
PerritoSaltador <32802770+PerritoSaltador@users.noreply.github.com>
2024-10-01 10:32:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Estoyusandoreact</Text>
+      <Text>Estoy usando react</Text>
       <StatusBar style="" />
     </View>
   );
